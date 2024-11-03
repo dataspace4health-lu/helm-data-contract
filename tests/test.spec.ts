a24f2cb25ea6697aab8ce0a1bfa04b6da263f794
wissem.bouhlel <wissem.bouhlel@global.ntt>
2024-11-03 17:22:01 +0100

    Integration of del and dct components and implementing tests

---
+++ tests/test.spec.ts
@@ -0,0 +1,197 @@
+import { test, expect, request } from "@playwright/test";
+import registerDataAsset from "./register.json";
+import providerNotification from "./provider-post-notification.json";
+import consumerNotification from "./consumer-post-notification.json";
+
+const dctBaseUrl = "http://dataspace4health.local/dct/api";
+
+test.describe("Data Contract Non Negotiable Flow", () => {
+  test("Service Health", async ({ request }) => {
+    const response = await request.get(`${dctBaseUrl}/health`);
+
+    // Validate status code
+    expect(response.status()).toBe(200);
+
+    // Parse the response JSON
+    const healthResponse = await response.json();
+
+    // Validate if the response contains Data Contract Signature
+    expect(healthResponse).toHaveProperty("status", "ok");
+  });
+  let registerAssetResponse;
+  test("Register a data asset", async ({ request }) => {
+    const response = await request.post(`${dctBaseUrl}/register`, {
+      headers: {
+        "Content-Type": " application/ld+json",
+      },
+      data: registerDataAsset,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(201);
+
+    // Parse the response JSON
+    registerAssetResponse = await response.json();
+
+    // Validate if the response contains Data Contract Signature
+    expect(registerAssetResponse).toHaveProperty("proof");
+  });
+
+  let finalContract;
+  test("Make contract", async ({ request }) => {
+    const consumerSignature = {
+      created: "2022-08-18T10:41:35Z",
+      jws: "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..Z3TevpLn-1MEc4hIdvmocc4uSWL-Y0xNoXuc8Pqp2H3u2N8SRC1QDr7oKE1lCiILf5B_npG61H4zWmuVcX0GBw",
+      proofPurpose: "assertionMethod",
+      type: "Ed25519Signature2018",
+      verificationMethod: "did:consumer:key:123",
+    };
+    const consumerDid = "did:consumer";
+    const consumerIssuer = "did:consumer:controller";
+
+    // Clone registerAssetResponse to avoid modifying the original data
+    const makeContractBody = {
+      ...registerAssetResponse,
+      verifiableCredential: [...registerAssetResponse.verifiableCredential],
+      proof: undefined,
+    };
+
+    // Add the consumer signature to the proof array
+    makeContractBody.verifiableCredential[0].proof.push(consumerSignature);
+    makeContractBody.verifiableCredential[0].credentialSubject["gax:consumer"] =
+      consumerDid;
+    makeContractBody.verifiableCredential[0].issuer = consumerIssuer;
+
+    const response = await request.post(`${dctBaseUrl}/make/contract`, {
+      headers: {
+        "Content-Type": " application/ld+json",
+      },
+      data: makeContractBody,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(201);
+
+    // Parse the response JSON
+    finalContract = await response.json();
+
+    // Validate if the response contains Data Contract final Signature
+    expect(finalContract).toHaveProperty("proof");
+  });
+
+  test("log contract token", async ({ request }) => {
+    const response = await request.post(`${dctBaseUrl}/log/token`, {
+      headers: {
+        "Content-Type": " application/ld+json",
+      },
+      data: finalContract,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(201);
+  });
+});
+
+const delBaseUrl = "http://dataspace4health.local/del/api";
+const providerAuthToken = process.env.PROVIDER_AUTH_TOKEN ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnYXgtZGNzOmxvZ0lEIjoiMjExNjUzNDktZTBiNy00NjYxLWFmYWYtNGE5ZWJmZGMwZGViIiwiZ2F4LWRjczpkYXRhVHJhbnNhY3Rpb25JRCI6IjEyMyIsImdheC1kY3M6Y29udHJhY3RJRCI6Imh0dHA6Ly9leGFtcGxlLm9yZy9kYXRhLWFzc2V0LTEiLCJpc3MiOiIoTG9nZ2luZyBzZXJ2aWNlIElEKSIsInN1YiI6ImRpZDpwcm92aWRlcjoxMjMiLCJhdWQiOiIoR1gtREVMUyBpZGVudGlmaWVyKSIsImV4cCI6MTY2MTI1NjQxMiwiaWF0IjoxNjYwODI0NDEyfQ.tWFCqb4daL7CQ7FC-c-dbIs5YDjRqzID-vMdkvuOP4E";
+  const consumerAuthToken = process.env.CONSUMER_AUTH_TOKEN ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJnYXgtZGNzOmxvZ0lEIjoiMjExNjUzNDktZTBiNy00NjYxLWFmYWYtNGE5ZWJmZGMwZGViIiwiZ2F4LWRjczpkYXRhVHJhbnNhY3Rpb25JRCI6IjEyMyIsImdheC1kY3M6Y29udHJhY3RJRCI6Imh0dHA6Ly9leGFtcGxlLm9yZy9kYXRhLWFzc2V0LTEiLCJpc3MiOiIoTG9nZ2luZyBzZXJ2aWNlIElEKSIsInN1YiI6ImRpZDpjb25zdW1lcjoxMjMiLCJhdWQiOiIoR1gtREVMUyBpZGVudGlmaWVyKSIsImV4cCI6MTY2MTI1NjQxMiwiaWF0IjoxNjYwODI0NDEyfQ.rUQtzr06Jd07qYzI-yTVn-YyG35_vMRu9spXtvljxEA";
+let adminToken;
+
+test.describe("Data Exchange Logging", () => {
+  test("Provider: Send notification", async ({ request }) => {
+    const response = await request.post(`${delBaseUrl}/inbox`, {
+      headers: {
+        "Content-Type": "application/ld+json",
+        Authorization: `Bearer ${providerAuthToken}`,
+      },
+      data: providerNotification,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(202);
+  });
+
+  test("Consumer: Send notification", async ({ request }) => {
+    const response = await request.post(`${delBaseUrl}/inbox`, {
+      headers: {
+        "Content-Type": "application/ld+json",
+        Authorization: `Bearer ${consumerAuthToken}`,
+      },
+      data: consumerNotification,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(202);
+  });
+
+  test("Get Notifications", async ({ request }) => {
+    const response = await request.get(`${delBaseUrl}/inbox`, {
+      headers: {
+        "Content-Type": "application/ld+json",
+        Authorization: `Bearer ${providerAuthToken}`,
+      },
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(200);
+
+    // Validate response body if it is an array
+    const responseBody = await response.json();
+    expect(Array.isArray(responseBody)).toBe(true);
+  });
+
+  test("Login to the admin portal", async ({ request }) => {
+    const loginBody = {
+      username: "admin",
+      password: "xfsc4Ntt!",
+    };
+    const response = await request.post(`${delBaseUrl}/auth/login`, {
+      data: loginBody,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(201);
+
+    // Parse the response JSON to get the Token
+    adminToken = (await response.json()).accessToken;
+  });
+
+  test("Get list of logs", async ({ request }) => {
+    const response = await request.get(`${delBaseUrl}/logs?page=1&pageSize=5`, {
+      headers: {
+        Authorization: `Bearer ${adminToken}`,
+      },
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(200);
+  });
+
+  test("Get list of settings", async ({ request }) => {
+    const response = await request.get(`${delBaseUrl}/settings`, {
+      headers: {
+        Authorization: `Bearer ${adminToken}`,
+      },
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(200);
+  });
+
+  test("update settings", async ({ request }) => {
+    const updatedSettings = {
+      SETTING_LOG_PRUNING_CRON: "*/4 * * * *",
+    };
+    const response = await request.put(`${delBaseUrl}/settings`, {
+      headers: {
+        Authorization: `Bearer ${adminToken}`,
+      },
+      data: updatedSettings,
+    });
+
+    // Validate status code
+    expect(response.status()).toBe(200);
+  });
+});
