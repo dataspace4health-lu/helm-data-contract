f097873128885e8e0426a5937ec7d480b7d82970
Antonio Santacaterina <antonio.santacaterina@global.ntt>
2025-06-22 15:40:38 +0200

    added test pipeline

--- jenkinsfile-test.groovy
+++ jenkinsfile-test.groovy
@@ -1,3 +1,10 @@
 @Library(value="ds4h", changelog=false) _
 
-helmPipeline()
+helmPipeline(
+    REPOS: [
+        HELM_PATH:'',
+        IMAGE_NAME:'xfsc-dct',
+        REPO_URL:'git@ssh.dev.azure.com:v3/Dataspace4Health/DS4H/xfsc-dct',
+        BRANCH:'main'
+    ]
+)
