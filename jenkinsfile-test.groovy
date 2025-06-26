6596b704db89f9336c8164692d0cd56afc443534
Antonio Santacaterina <antonio.santacaterina@global.ntt>
2025-06-26 20:50:14 +0200

    improved test pipeline

--- jenkinsfile-test.groovy
+++ jenkinsfile-test.groovy
@@ -3,12 +3,14 @@
 properties([
     parameters([
         string(name: 'NODE_NAME', description: 'Agent to run on', defaultValue: 'any'),
+        string(name: 'BRANCH', description: 'Branch to check out', defaultValue: ''),
         string(name: "PUBLISH_HELM", description: "Registry to use for publishing", defaultValue: '')
     ])
 ])
 
 helmPipeline(
     NODE_NAME: params.NODE_NAME,
+    BRANCH: params.BRANCH,
     IMG_PAHTS: 'select(.kind == "Deployment" and .metadata.labels."app.kubernetes.io/name" == "dct") | .spec.template.spec.containers[0].image',
     IMG_JOBS: 'DS4H/test-antonio-docker/xfsc-dct',
     PUBLISH_HELM: params.PUBLISH_HELM
